619ebe06741de0d65e06aaee2715ef2313ab2c6c
Alhassan Joshua Abel <saintjosh26@gmail.com>
2023-02-07 00:34:49 +0000

    Looped through each sibling element

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const smallCups = document.querySelectorAll('.cup-small');
 const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
+
+// Loop through Cups and add Event listeners
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highLightCup(idx))
+})
+
+function highLightCup(idx) {
+    if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+
+    smallCups.forEach((cup, idx2) => {
+        if(idx2 <= idx) {
+            cup.classList.add('full')
+        } else {
+            cup.classList.remove('full')
+        }
+    })
+}
